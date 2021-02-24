53911b8cc64a74e782054107bdbf0ee41b1b4cf1
taimoor954 <taimoormuhammad954@gmail.com>
2021-02-24 18:07:39 +0500

    only log remaining

--- index.js
+++ index.js
@@ -1,51 +1,84 @@
 //work on health bar of player and monster
 //buttons like attack, giveup , heal , special attack , new game
-//log the attack and everyting 
+//log the attack and everyting
 
 new Vue({
-    el : "#app",
-    data : {
-        playerHealth  : 100,
-        monsterHealth  : 100,
-        gameIsRunning : false,
+  el: "#app",
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunning: false,
+  },
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+    },
+    attack: function () {
+      this.monsterHealth -= this.calculateDamage(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
 
+      this.playerHealth -= this.calculateDamage(2, 8);
+      this.checkWin();
+    },
+    specialAttack: function () {
+      this.monsterHealth -= this.calculateDamage(10, 20);
+      if (this.checkWin()) {
+        return;
+      }
+
+      this.monsterAttacks();
+    },
+    monsterAttacks: function () {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        this.monsterHealth = 0;
+        if (confirm("You won! New game")) {
+          this.monsterHealth = 0;
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        this.playerHealth = 0;
+        if (confirm("You lost! New game")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+    heal : function()
+    {
+        if(this.playerHealth<=90)
+        {
+            this.playerHealth += 10
+        }
+        else{
+            this.playerHealth=100
+        }
+        this.monsterAttacks()
 
     },
-    methods : {
-        startGame : function()
+    giveUp : function()
+    {
+        this.playerHealth = 0
+        if(this.checkWin())
         {
-            this.gameIsRunning = true
-            this.playerHealth = 100
-            this.monsterHealth =100
-
-        },
-        attack : function()
-        {
-           var max = 10
-           var min = 3
-           var damage = Math.max(Math.floor(Math.random()*max)+1,min)
-           this.monsterHealth -=damage 
-           if(this.monsterHealth<=0)
-           {
-               alert('You won')
-               this.gameIsRunning =false
-               return
-           }
-
-           var max = 8
-           var min = 2
-           var damage = Math.max(Math.floor(Math.random()*max)+1,min)
-           this.playerHealth -=damage 
-           if(this.playerHealth<=0)
-           {
-               alert('Monster won')
-               this.gameIsRunning =false
-               return
-           }
-
-        },
-        specialAttack : function () {
-
+            return
         }
     }
-})
+  },
+});
