3461e754d89e687273e3562619ae4b2afd656103
taimoor954 <taimoormuhammad954@gmail.com>
2021-02-24 15:42:09 +0500

    done with start button

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ new Vue({
         playerHealth  : 100,
         monsterHealth  : 100,
         gameIsRunning : false,
-        
 
+
+    },
+    methods : {
+        startGame : function()
+        {
+            this.gameIsRunning = true
+            this.playerHealth = 100
+            this.monsterHealth =100
+
+        }
     }
 })
