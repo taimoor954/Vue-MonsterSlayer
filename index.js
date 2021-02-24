03e0dd1ad62be71ce5ea695cee83a6e49b04d0fa
taimoor954 <taimoormuhammad954@gmail.com>
2021-02-24 15:28:17 +0500

    MonsterSlayer App health bar progess

---
+++ index.js
@@ -0,0 +1,14 @@
+//work on health bar of player and monster
+//buttons like attack, giveup , heal , special attack , new game
+//log the attack and everyting 
+
+new Vue({
+    el : "#app",
+    data : {
+        playerHealth  : 100,
+        monsterHealth  : 100,
+        gameIsRunning : false,
+        
+
+    }
+})
